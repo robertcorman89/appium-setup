2c12ba246856817b7d22ff72d6d7c14b967b1c8e
Robert Corman <34003401+robertcorman89@users.noreply.github.com>
2021-02-24 17:08:30 +0200

    Update Capabilities.java

--- src/main/java/framework/data/Capabilities.java
+++ src/main/java/framework/data/Capabilities.java
@@ -6,6 +6,6 @@ public class Capabilities {
 	public static final int DEVICE_ID = 123456;
 	public static final int ANDROID_VERSION = 10;
 	public static final String APP_NAME = "app-qc.apk";
-	public static final String APP_ACTIVITY = "com.datamine.discovermobile.activities.landingpage.LandingPageActivity";
+	public static final String APP_ACTIVITY = "";
 	public static final String EMULATOR_NAME = "Pixel_4_API_28";
 }
