5724689adf96841d06977eea13c65999698b2c0f
Robert Corman <34003401+robertcorman89@users.noreply.github.com>
2021-02-24 17:08:56 +0200

    Update Environment.java

--- src/main/java/framework/data/Environment.java
+++ src/main/java/framework/data/Environment.java
@@ -4,5 +4,5 @@ public class Environment {
 	public static final int PORT = 4723;
 	public static final String URL = "http://127.0.0.1";
 	public static final String IP = "127.0.0.1";
-	public static final String APPIUM_JS_PATH = "C:\\Users\\robertc\\AppData\\Roaming\\npm\\node_modules\\appium";
+	public static final String APPIUM_JS_PATH = "";
 }
